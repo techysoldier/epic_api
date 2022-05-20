4bb5337c27ce938aab7ef9e729d2ba864b157ddb
devin <devinsymone@gmail.com>
2022-05-20 12:58:01 -0400

    serializers, and views function for get comments for reviews

--- backend/epic/serializers.py
+++ backend/epic/serializers.py
@@ -1,10 +1,17 @@
+from dataclasses import field
 from pyexpat import model
 from rest_framework import serializers
 from .models import Comment
-from .models import Reply
+from .models import MemberCenter
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
+
+class MemberCenterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MemberCenter
+        field = ['userid', 'name','email', 'membersince']
+        depth = 1 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
