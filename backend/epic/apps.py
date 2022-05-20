0c582135dba5166b8fb20ce9aec4e8dd9e15506c
devin <devinsymone@gmail.com>
2022-05-20 12:24:22 -0400

    application creation

---
+++ backend/epic/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EpicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'epic'
---
+++ backend/epic/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+
+class MemberCenter(models.Model):
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    membersince= models.IntegerField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    locationid = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+
