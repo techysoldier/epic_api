a0ea5db2533fda0c1cfad31a9434651197eb568d
devin <devinsymone@gmail.com>
2022-05-25 15:27:36 -0400

    Added map. Need to figure out why key is not functional. Requesting assitance from instructor

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,74 +1,111 @@
-import React from "react";
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import {KEY} from '../../localKey'
 
-
-const Maps = (props) => {
-let service;
-const libraries = ["places"];
-
-const mapContainerStyle = {
-  height: "100vh",
-  width: "100vw"
+const containerStyle = {
+  width: '400px',
+  height: '400px'
 };
-const options = {
-  disableDefaultUI: true,
-  zoomControl: true
-};
+
 const center = {
-  lat: 43.6532,
-  lng: -79.3832
+  lat: -3.745,
+  lng: -38.523
 };
 
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: {KEY},
-    libraries
-  });
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback(map => {
-    mapRef.current = map;
-  }, []);
-
-  const panTo = React.useCallback(({ lat, lng }) => {
-    mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(12);
-    let map = mapRef.current;
-
-    let request = {
-      location: { lat, lng },
-      radius: "500",
-      type: ["places"]
-    };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    service = new GoogleMap.maps.places.PlacesService(mapRef.current);
-    service.nearbySearch(request, callback);
-    function callback(results, status) {
-      if (status === GoogleMap.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i++) {
-          let place = results[i];
-          new GoogleMap.maps.Marker({
-            position: place.geometry.location,
-            map
-          });
-        }
-      }
-    }
-  }, []);
-
+function MyComponent() {
   return (
-    <div>
+    <LoadScript
+      googleMapsApiKey={KEY}
+    >
       <GoogleMap
-        id="map"
-        mapContainerStyle={mapContainerStyle}
-        zoom={8}
+        mapContainerStyle={containerStyle}
         center={center}
-        options={options}
-        onLoad={onMapLoad}
-      />
-    </div>
-  );
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default Maps;
+export default React.memo(MyComponent)
+
+
+
+
+
+// import React from "react";
+// import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+// import {KEY} from '../../localKey'
+
+
+// const Maps = (props) => {
+// let service;
+// const libraries = ["places"];
+
+// const mapContainerStyle = {
+//   height: "100vh",
+//   width: "100vw"
+// };
+// const options = {
+//   disableDefaultUI: true,
+//   zoomControl: true
+// };
+// const center = {
+//   lat: 43.6532,
+//   lng: -79.3832
+// };
+
+//   const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: {KEY},
+//     libraries
+//   });
+//   const mapRef = React.useRef();
+//   const onMapLoad = React.useCallback(map => {
+//     mapRef.current = map;
+//   }, []);
+
+//   const panTo = React.useCallback(({ lat, lng }) => {
+//     mapRef.current.panTo({ lat, lng });
+//     mapRef.current.setZoom(12);
+//     let map = mapRef.current;
+
+//     let request = {
+//       location: { lat, lng },
+//       radius: "500",
+//       type: ["places"]
+//     };
+
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//     service = new GoogleMap.maps.places.PlacesService(mapRef.current);
+//     service.nearbySearch(request, callback);
+//     function callback(results, status) {
+//       if (status === GoogleMap.maps.places.PlacesServiceStatus.OK) {
+//         for (let i = 0; i < results.length; i++) {
+//           let place = results[i];
+//           new GoogleMap.maps.Marker({
+//             position: place.geometry.location,
+//             map
+//           });
+//         }
+//       }
+//     }
+//   }, []);
+
+//   return (
+//     <div>
+//       <GoogleMap
+//         id="map"
+//         mapContainerStyle={mapContainerStyle}
+//         zoom={8}
+//         center={center}
+//         options={options}
+//         onLoad={onMapLoad}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Maps;
 
